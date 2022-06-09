3e22a0dbd9decb5a8f127d54ceb51c0c25160857
sochubert <sochubert@gmail.com>
2022-06-09 14:17:08 +0900

    frontend init

---
+++ frontend/src/products.js
@@ -0,0 +1,82 @@
+const products = [
+  {
+    _id: "1",
+    name: "Grandma's Cookies Variety Pack of 30",
+    image: "/images/cookie.jpg",
+    description:
+      "This variety pack of Grandma's cookies offers a great selection of our most popular flavors including Chocolate Chip, Oatmeal Raisin, Peanut Butter, Chocolate Brownie, Mini Chocolate Chip cookies and Vanilla Sandwich Cremes. These snacks are perfect for packing in a lunch, enjoying in the car, after school, or even at work. With 30 individually wrapped packs, there's something for everyone to enjoy. Grandma's cookies offer a delicious variety of homemade memories. Every Grandma's treat is baked with love to transport you to your happy place with every bite.",
+    brand: "Grandma's",
+    category: "cookie",
+    price: 89.99,
+    countInStock: 0,
+    rating: 4.5,
+    numReviews: 4,
+  },
+  {
+    _id: "2",
+    name: "Sugar-Free Pre-Workout Energy Supplement Drink",
+    image: "/images/vitamin.jpg",
+    description:
+      "Total Workout Energy: ZOA+ Pre-Workout Energy Supplements provide an extra boost of healthy energy support for sugar free on the go energy to promote focus and support powerful, productive, and positive workouts.",
+    brand: "ZOA",
+    category: "Drink",
+    price: 39.99,
+    countInStock: 7,
+    rating: 4.0,
+    numReviews: 8,
+  },
+  {
+    _id: "3",
+    name: "Welch's Fruit Snacks, Mixed Fruit,",
+    image: "/images/snack.jpg",
+    description:
+      "Welch's Fruit Snacks Mixed Fruit. Family farmer owned.80 Calorie pouches. Fruit is our 1st ingredient. Natural & artificial flavors. Made with real fruit. DV per serving: 100% Vitamin C. 25% Vitamins A & E. Fat free. Gluten free. No preservatives.",
+    brand: "Welch's",
+    category: "Snack",
+    price: 20.99,
+    countInStock: 5,
+    rating: 3,
+    numReviews: 3,
+  },
+  {
+    _id: "4",
+    name: "Rao's Homemade Marinara Sauce, 24 oz, All Purpose Tomato Sauce",
+    image: "/images/sauce.jpg",
+    description:
+      "Bring home the famous taste of Rao’s Homemade Marinara Sauce. Rao’s Homemade Marinara Sauce is a premium, carb conscious marinara sauce made with only the finest ingredients. Delicious speaks for itself when enjoying this marinara sauce. Each batch of Rao’s Homemade Marinara Sauce is slow-cooked in small batches with high quality ingredients.",
+    brand: "Sony",
+    category: "Electronics",
+    price: 399.99,
+    countInStock: 11,
+    rating: 5,
+    numReviews: 6,
+  },
+  {
+    _id: "5",
+    name: "Spam Classic, 12 Ounce Can (Pack of 12)",
+    image: "/images/spam.jpg",
+    description:
+      "Don’t knock it till you’ve fried it! SPAM Classic is the original and timeless spiced meat that’s brought a delicious taste of variety for generations. There’s a reason that 12.8 cans of SPAM products are eaten every second. You can enjoy this fully cooked spiced meat hot or cold, making it perfect for home cooked meals, family get-togethers, or even camping food. Get creative with the SPAM brand and experience the flavor today! SIZZLE PORK AND MMM. SPAM is a trademark of Hormel Foods, LLC.",
+    brand: "SPAM",
+    category: "food",
+    price: 40.5,
+    countInStock: 7,
+    rating: 3.5,
+    numReviews: 7,
+  },
+  {
+    _id: "6",
+    name: "China Good Food 量贩装China instant noodles（康师傅 经典鲜虾鱼板面 5 bags）",
+    image: "/images/china.jpg",
+    description:
+      "China Good Food 量贩装China instant noodles（康师傅 经典鲜虾鱼板面 5 bags）康師傅/Masterkong方便面 泡面 速食面 中国人的方便面 免运费",
+    brand: "China Good Food",
+    category: "chinese noodle",
+    price: 29.99,
+    countInStock: 0,
+    rating: 4,
+    numReviews: 3,
+  },
+];
+
+export default products;
