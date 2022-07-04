0fd67ab0345dd4e1caefa4b71b4eb54b8a0ad649
sochubert <sochubert@gmail.com>
2022-07-04 14:17:31 +0900

    footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -7,7 +7,13 @@ const Footer = () => {
       <Container>
         <Row>
           <Col className="text-center py-3">
-            Copyright &copy; 2022 HeadingWarm
+            Copyright &copy; 2022 HeadingWarm <br />
+            <br />
+            상호명 : 헤딩웜 | 사업자등록번호 : 845-08-01863 <br />
+            통신판매업 : 제 2022-경기안산-1988 호 | 대표 : 정지윤 <br />
+            주소 : 경기도 안산시 상록구 한양대학로 55, 제5공학관 창업실 4호실
+            <br />
+            이메일 : headingwarm10@gmail.com
           </Col>
         </Row>
       </Container>
